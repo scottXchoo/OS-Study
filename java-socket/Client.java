662fc9e2c83a148f4e4da81191085a517386d102
scottXchoo <ckh0601@cau.ac.kr>
2024-03-29 13:18:49 +0900

    Server & Client 클래스 생성 및 소켓 통신

--- java-socket/Client.java
+++ java-socket/Client.java
@@ -1,2 +1,25 @@
+import java.io.*;
+import java.net.*;
+
 public class Client {
+    public static void main(String[] args) {
+        try {
+            /* make connection to server socket */
+            Socket socket = new Socket("127.0.0.1", 6666);
+
+            InputStream in = socket.getInputStream();
+            BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+
+            /* read the date from the socket */
+            String line;
+            while ((line = bin.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            /* close the socket communication */
+            socket.close();
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
 }
--- java-socket/Server.java
+++ java-socket/Server.java
@@ -1,2 +1,24 @@
+import java.io.*;
+import java.net.*;
+
 public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket socket = new ServerSocket(6666);
+
+            /* new listen for connections */
+            Socket client = socket.accept();
+
+            PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
+
+            /* write the Data to the socket */
+            pout.println(new java.util.Date());
+
+            /* close the socket and reuse */
+            /* listening for connections */
+            client.close();
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
 }
